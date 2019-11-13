ba1931c9a2361d19f0666486e1dcfbd514ae9861
mortentang <mortentc@hotmail.com>
2019-11-13 17:01:58 +0100

    Updates

---
+++ precache-manifest.8b3e29a2b589ce5e88e7bed696aad07b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818364942a871881cbb3d368f83223ec",
+    "url": "/scrapeit/index.html"
+  },
+  {
+    "revision": "d46213900a873916b921",
+    "url": "/scrapeit/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a65c1fce81a6577e289",
+    "url": "/scrapeit/static/js/2.07ff2068.chunk.js"
+  },
+  {
+    "revision": "d46213900a873916b921",
+    "url": "/scrapeit/static/js/main.83eb10d8.chunk.js"
+  },
+  {
+    "revision": "3b58e60c15706b9159d2",
+    "url": "/scrapeit/static/js/runtime-main.ae890a62.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/scrapeit/static/media/logo.25bf045c.svg"
+  }
+]);

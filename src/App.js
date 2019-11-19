fe90a158dd5091c84a92134030c7203db3e58f90
mortentang <mortentc@hotmail.com>
2019-11-19 21:03:44 +0100

    added bootstrap created navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import logo from "./logo.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="white" variant="light">
+          <Navbar.Brand href="/" >
+              <img src={logo} className="Logo"/>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+              <Nav.Link href="/">Intro</Nav.Link>
+              <Nav.Link href="/analysis">Analysis</Nav.Link>
+              <Nav.Link href="/links">Links</Nav.Link>
+          </Nav>
+      </Navbar>
+        <div className="Body">
+            <p>
+              <b> WELCOME TO SCRAPEIT</b>
+            </p>
+        </div>
     </div>
   );
 }
